6910c42ee9b7ba61d309eabccb9945749bb50920
MazumderDip <mazumder.dip.auvi@gmail.com>
2019-09-03 17:57:20 +0600

    Initial commit

--- Fashion_MNIST/main.py
+++ Fashion_MNIST/main.py
@@ -13,11 +13,6 @@ fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-print(train_images.shape)
-len(train_labels)
-print(train_labels)
-print(test_images.shape)
-len(test_labels)
 
 """plt.figure()
 plt.imshow(train_images[0])
